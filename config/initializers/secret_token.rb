5a423868ba2394ab65437d77680f5df946cd4c35
Kevin Scheunemann <kevin@nanbox.net>
2012-10-08 05:36:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gfgapi::Application.config.secret_token = '4d0b4b837ef027f0d287c59f7dbf2cc5a5deced9e333d0dc0363c3b5bb004f2d967faa361624064141dd56e4c7ae44aa9b682823a5464e54461621535ae53cae'
