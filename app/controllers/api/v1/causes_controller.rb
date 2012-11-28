5ed9de176375cddc031bce3db60afa78ace27cd9
Kevin Scheunemann <kevin@nanbox.net>
2012-11-27 22:00:09 -0600

    added

--- app/controllers/api/v1/causes_controller.rb
+++ app/controllers/api/v1/causes_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::CausesController < ApplicationController
     @causes = Cause.all.to_a
     respond_with @causes
   end
+
+  def show
+    cause = Cause.find params[:id]
+    respond_with cause
+  end
+
+  def update
+    cause = Cause.find params[:id]
+    if task.update_attributes params[:task]
+    else
+    end
+  end
 end
