5ad2f858d35965afe1bbfe18792bf87e3fed4512
Kevin Scheunemann <kevin@nanbox.net>
2013-03-01 13:26:08 -0600

    make default true

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -11,7 +11,7 @@ class Cause
   field :type, type: String
   field :startDate, type: Date
   field :endDate, type: Date
-  field :active, type: Boolean
+  field :active, type: Boolean, default: true
   field :isFeatured, type: Boolean
   field :donations, type: Float, default: 0
   field :createdBy, type: String
